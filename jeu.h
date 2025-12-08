1e858baa3a86f43e194894afaf1a0fbdd76a6909
celinemrc2007 <celine.marco92@gmail.com>
2025-12-08 20:05:03 +0100

    Add eliminerFigure function declaration

--- jeu.h
+++ jeu.h
@@ -2,4 +2,6 @@ eliminerSuiteEnLigne(int ligne, int colonne, int taille);
 eliminerSuiteEnColonne(int ligne, int colonne, int taille);
 eliminerCarre (int ligne, int colonne);
 eliminerCroix (int ligne, int colonne);
+eliminerFigure (int type, int ligne, int colonne, int taille);
+
 
