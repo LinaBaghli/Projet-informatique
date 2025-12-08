c5c6e66a43e863ecc4c4d0c6bec4878ecd92e541
celinemrc2007 <celine.marco92@gmail.com>
2025-12-08 20:04:32 +0100

    Add eliminerCroix function declaration

--- jeu.h
+++ jeu.h
@@ -1,3 +1,5 @@
 eliminerSuiteEnLigne(int ligne, int colonne, int taille);
 eliminerSuiteEnColonne(int ligne, int colonne, int taille);
 eliminerCarre (int ligne, int colonne);
+eliminerCroix (int ligne, int colonne);
+
